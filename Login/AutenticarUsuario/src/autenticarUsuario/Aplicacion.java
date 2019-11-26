3a483dc9f24ea53a4a51456ea641b8726e3e7d7d
bravinjuan <bravinjuangmail.com>
2019-11-26 16:05:04 -0300

    prueba

---
+++ Login/AutenticarUsuario/src/autenticarUsuario/Aplicacion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autenticarUsuario;
+import modelo.Usuario;
+import modelo.Sistema;
+import pantallas.pantallaLogin;
+/**
+ *
+ * @author Juan
+ */
+public class Aplicacion {
+    
+    public static void main(String[] args){
+        Sistema sistema = new Sistema();
+        Connector c = new Connector();
+        c.crearTabla();
+        //c.insertarUsuario("JorgeT", "12345", "tecnico");
+        //c.insertarUsuario("JulianG","maxElec", "postventa");
+        //c.insertarUsuario("VictorG","2019","gerente");
+        c.getUsuarios(sistema);
+        AutenticarUsuarioController ctrl = new AutenticarUsuarioController(sistema);
+        ctrl.iniciarPantalla();
+    }
+}
